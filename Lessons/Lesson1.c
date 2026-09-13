8fb41657539adc7035869f8419e47abfc1320199
cGradying <cGradying@gmail.com>
2026-09-13 08:26:40 +0000

    my final update on lesson 1 | Introduction

--- Lessons/Lesson1.c
+++ Lessons/Lesson1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello world\n");
+    printf("Hello my name is Janvin\n");
+    printf("Heres the 3 thing I like\n");
+    printf("1. Ice Cream\n");
+    printf("2. Video Games\n");
+    printf("3. Reading Books\n");
     return 0;
 }
