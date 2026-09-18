c7df4e1f57501e084efb826fff4effe7e6db2e18
cGradying <cGradying@gmail.com>
2026-09-18 17:17:24 +0000

    My first hello.c

--- Lessons/Lesson1.c
+++ Lessons/Lesson1.c
@@ -1,10 +1,22 @@
+/*
+ * Program Name : hello.c
+ * Description  : Verification that the C toolchain is correctly set up.
+ * Author       : Janvin D. Salvador
+ * Section      : ASCS 1-IN
+ * Date Created : 2026-09-13
+ * Date Modified: 2026-09-13
+ * Course       : COMP 002 - Computer Programming 1
+ * Instructor   : Prof. Jayson James Mayor
+ */
+
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello my name is Janvin\n");
-    printf("Heres the 3 thing I like\n");
-    printf("1. Ice Cream\n");
-    printf("2. Video Games\n");
-    printf("3. Reading Books\n");
+    printf("COMP 002 - Computer Programming 1\n");
+    printf("My toolchain is working.\n");
+    
+    printf("\nPress enter to exit...");
+    getchar();
+    
     return 0;
 }
